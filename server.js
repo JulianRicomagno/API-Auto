82cfccca658eaf8db343f02dd17695601b302119
diego boya <dboyatjian@gmail.com>
2021-06-02 22:22:11 -0300

    coreccion del server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const historicoAutosAlquiladosRoutes = require('./src/routes/historicoAutosAlqui
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin,     X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-    Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT,     DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
+
 app.use('/api/autos', autosRoutes);
 app.use('/api/estacionamientos', estacionamientosRoutes);
 app.use('/api/users', usersRoutes);
