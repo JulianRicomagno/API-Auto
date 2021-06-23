b4796e19c55644a8e3f241892b006165dadfa1fb
diego boya <dboyatjian@gmail.com>
2021-06-23 02:09:56 -0300

    modificacion es ESTACIONAMIENTOS model y schema para poder realizar el llamado de los autos que se encuentran en un estacionamiento segun el ID pasado por parametro. metodo getCars

--- databases/mongo/models/estacionamientos.js
+++ databases/mongo/models/estacionamientos.js
@@ -4,6 +4,9 @@ const { Schema } = mongoose;
 const schema = new Schema ({
     name: {type: String, required: true},
     auto: [{type: Schema.Types.ObjectId, ref: 'Autos', autopopulate: true}],
+    image: {type: String, required: true},
+    location:{type: String, required: true},
+    description: {type: String, required: true}
 }, { timestamps: true })
 
 schema.plugin(require('mongoose-autopopulate'));
